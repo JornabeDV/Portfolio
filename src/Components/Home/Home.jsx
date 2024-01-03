5aa1c4d36a590b981cd6ce3fba2e008b0c3e313f
Jorge Nahuel Beja Rosa <jorgebejarosa@gmail.com>
2024-01-03 11:28:25 -0300

    [FIX] Change link my proyects

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,21 +1,21 @@
-import "./Home.css";
+import './Home.css'
 
 const Home = () => {
   return (
-    <div id="intro">
-      <div className="intro-overlay"></div>
-      <div className="intro-content">
+    <div id='intro'>
+      <div className='intro-overlay' />
+      <div className='intro-content'>
         <h5>Hello, World</h5>
         <h1>I'm Jorge Nahuel</h1>
-        <p className="intro-position">
+        <p className='intro-position'>
           <span>Software Developer</span>
         </p>
-        <a className="button" href="#about" title="">
-          More About Me
+        <a className='button' href='#resume' title=''>
+          My Proyects
         </a>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
